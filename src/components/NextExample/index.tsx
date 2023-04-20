488a0835972fe7fc77a829f50358328f0fac3ea9
weareoutman <wangshenwei@qq.com>
2023-04-20 17:16:48 +0800

    WIP: drop react-tabs

--- src/components/NextExample/index.tsx
+++ src/components/NextExample/index.tsx
@@ -2,12 +2,21 @@ import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import { useColorMode } from '@docusaurus/theme-common';
 import useDeferredValue from '@site/src/hooks/useDeferredValue';
-import FileTabs, { FileInfo } from '@site/src/components/FileTabs';
+import Editor from 'react-simple-code-editor';
+import { highlight, languages } from 'prismjs/components/prism-core';
+import 'prismjs/components/prism-yaml';
+import 'prismjs/themes/prism.css';
+import clsx from 'clsx';
 import styles from './styles.module.css';
 
 export interface NextExampleProps {
   files: FileInfo[];
   defaultFile?: string;
+}
+
+export interface FileInfo {
+  name: string;
+  code: string;
 }
 
 const MIN_HEIGHT = 32;
@@ -18,12 +27,13 @@ export default function NextExample({ files, defaultFile }: NextExampleProps): J
   const iframeRef = useRef<HTMLIFrameElement>();
   const [iframeHeight, setIframeHeight] = useState(MIN_HEIGHT);
   const [ready, setReady] = useState(false);
+  const [currentFile, setCurrentFile] = useState(() => defaultFile ?? files[0].name);
 
   const handleIframeLoad = useCallback(() => {
     setReady(true);
   }, []);
 
-  const [codeByFile, setCodeByFile] = useState<Record<string, string>>({});
+  const [codeByFile, setCodeByFile] = useState<Record<string, string>>(() => Object.fromEntries(files.map(f => [f.name, f.code])));
 
   const deferredBricks = useDeferredValue(codeByFile.Bricks);
   const deferredContext = useDeferredValue(codeByFile.Context);
@@ -67,16 +77,51 @@ export default function NextExample({ files, defaultFile }: NextExampleProps): J
     };
   }, [ready]);
 
+  const handleCodeChange = useCallback((code: string) => {
+    setCodeByFile((prev) => ({
+      ...prev,
+      [currentFile]: code,
+    }));
+  }, [currentFile]);
+
   return (
     <div className={styles.example}>
-      <FileTabs
-        files={files}
-        defaultFile={defaultFile}
-        onChange={setCodeByFile}
-      />
-      <div className={styles.previewContainer}>
-        <div className={styles.preview}>
-          <iframe ref={iframeRef} src={previewSrc} onLoad={handleIframeLoad} style={{height: iframeHeight}} />
+      <div className={styles.tabs}>
+        {
+          files.map(file => (
+            <div className={clsx(styles.tab, {[styles.active]: file.name === currentFile})} key={file.name} onClick={() => {
+              setCurrentFile(file.name);
+            }}>{file.name}</div>
+          ))
+        }
+      </div>
+      <div className={styles.editorAndPreview}>
+        <div className={styles.editorColumn}>
+          <div className={styles.editorContainer}>
+            <Editor
+              value={codeByFile[currentFile]}
+              onValueChange={handleCodeChange}
+              highlight={
+                code =>
+                  highlight(code, languages.yaml)
+                    .split('\n')
+                    .map((line, i) => `<span class="${styles.lineNumber}">${i + 1}</span>${line}`)
+                    .join('\n')
+              }
+              padding={16}
+              textareaClassName={styles.codeTextarea}
+              preClassName={styles.codePre}
+              style={{
+                fontFamily: 'var(--ifm-font-family-monospace)',
+                fontSize: 13,
+              }}
+            />
+          </div>
+        </div>
+        <div className={styles.previewColumn}>
+          <div className={styles.preview}>
+            <iframe ref={iframeRef} src={previewSrc} onLoad={handleIframeLoad} style={{height: iframeHeight}} />
+          </div>
         </div>
       </div>
     </div>
