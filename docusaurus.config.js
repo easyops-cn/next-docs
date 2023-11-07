725f02e995dfa6cd25d1fac2e7349fdf7549770c
weareoutman <wangshenwei@qq.com>
2023-11-07 11:33:03 +0800

    Update footer links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -187,16 +187,8 @@ const config = {
             title: "Community",
             items: [
               {
-                label: "Stack Overflow",
-                href: "https://stackoverflow.com/questions/tagged/docusaurus",
-              },
-              {
-                label: "Discord",
-                href: "https://discordapp.com/invite/docusaurus",
-              },
-              {
-                label: "Twitter",
-                href: "https://twitter.com/docusaurus",
+                label: "Bricks",
+                href: "https://bricks.js.org/",
               },
             ],
           },
